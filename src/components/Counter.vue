8f5e1e4ba5d1517f75da454d7b4b053cb1c47455
ferretdayo <ferretdayo@hotmail.co.jp>
2017-12-18 22:46:44 +0900

    init

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="counter">
+    <div>{{count}}</div>
+    <button v-on:click="decrement">-1</button>
+    <button v-on:click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Counter',
+  computed: {
+    count () {
+      return this.$store.getters.getCount
+    }
+  },
+  methods: {
+    ...mapActions({
+      increment: 'increment',
+      decrement: 'decrement'
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
