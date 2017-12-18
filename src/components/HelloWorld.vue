98e9ed70370e7deacc576d4b59c30db286729fbd
ferretdayo <ferretdayo@hotmail.co.jp>
2017-12-18 22:46:44 +0900

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './Counter'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
