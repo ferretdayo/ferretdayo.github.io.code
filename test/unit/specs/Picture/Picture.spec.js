5d516241086c22636537ebc34fa1aacf273dd5cd
ferretdayo <ferretdayo@hotmail.co.jp>
2018-02-03 10:53:39 +0900

    fix

--- test/unit/specs/Picture/Picture.spec.js
+++ test/unit/specs/Picture/Picture.spec.js
@@ -36,13 +36,26 @@ describe('Picture.vue', () => {
   })
 
   it('should have selected Picture', () => {
-    const picture = { name: '_eno1.jpg', caption: 'ミズゴロウと岩' }
-    store.dispatch('picture/tapPicture', picture)
-    expect(store.getters['picture/getSelectedPicture']).toBe(picture)
+    const wrapper = shallow(Picture, { store, localVue })
+    const img = wrapper.findAll('.image').at(0)
+    const picture = { name: '__waka1.jpg', caption: '白浜海岸のカニ' } // 0番目の写真
+    img.trigger('click')
+    expect(store.getters['picture/getSelectedPicture']).toEqual(picture)
     expect(store.getters['picture/getIsOpenDialog']).toBe(true)
   })
 
   it('should have false isOpenDialog', () => {
+    // const wrapper = shallow(Picture, { store, localVue })
+    // 状態が初期化されないので，ひとまず．．．これ
+    // store.state.picture.isOpenDialog = false
+
+    // クリックして，ダイアログを出す
+    // const img = wrapper.findAll('.image').at(0)
+    // img.trigger('click')
+
+    // ダイアログを閉じる
+    // const dialog = wrapper.find('.dialog')
+    // dialog.trigger('input')
     store.dispatch('picture/closeDialog')
     expect(store.getters['picture/getIsOpenDialog']).toBe(false)
   })
