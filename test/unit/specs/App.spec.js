3cf91c70628d6cf1acf1bf2274cf4033621e3510
ferretdayo <ferretdayo@hotmail.co.jp>
2018-02-03 11:00:47 +0900

    headerを追加しないと．．．

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -3,6 +3,8 @@ import Vuetify from 'vuetify'
 import { shallow, createLocalVue } from 'vue-test-utils'
 
 import App from '@/App'
+import Header from '@/components/Header'
+
 import CommonAction from '@/vuex/Common/CommonAction'
 import CommonGetter from '@/vuex/Common/CommonGetter'
 import CommonMutation from '@/vuex/Common/CommonMutation'
@@ -31,9 +33,8 @@ describe('App.vue', () => {
   })
 
   it('should render correct contents', () => {
-    // const wrapper = shallow(App, { store, localVue })
-    // const mizugorouImage = wrapper.find('img')
-    // console.log(mizugorouImage)
-    // expect(wrapper.element.querySelector('h1').textContent).toEqual('Picture!')
+    const wrapper = shallow(App, { store, localVue })
+    const mizugorouImage = wrapper.find('img')
+    expect(mizugorouImage.exists()).toBe(true)
   })
 })
