c16cf470016d09fba5ac9b3be361011473b5c62e
ferretdayo <ferretdayo@hotmail.co.jp>
2018-02-17 10:42:52 +0900

    hohoge

--- src/vuex/Resas/_api/index.ts
+++ src/vuex/Resas/_api/index.ts
@@ -2,14 +2,13 @@ import axios from 'axios'
 
 export default {
   getPeople (): Promise<any> {
-    console.log(this)
     return axios.get('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear', {
       params: {
         cityCode: 11362,
         prefCode: 11
       },
       headers: {
-        // 'X-API-KEY': 
+        'X-API-KEY': process.env.RESAS_API_KEY
       }
     })
   }
